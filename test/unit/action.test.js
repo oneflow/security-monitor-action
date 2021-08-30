a9141576350f86f3d37c33435a4d025741fcd738
alesiaviktorchyk <45420854+alesiaviktorchyk@users.noreply.github.com>
2021-08-30 16:42:13 +0000

    Revert "fix: empty pr description"

--- test/unit/action.test.js
+++ test/unit/action.test.js
@@ -1,52 +1,7 @@
-const { issuesMessage, findStatus } = require('../../src/github/action');
+const { issuesMessage } = require('../../src/github/action');
 const repoInfo = require('./fixtures/graphql/repoInfo');
 
 test('check message', async () => {
 	const message = await issuesMessage(repoInfo.data.repository, repoInfo.data.repository.vulnerabilityAlerts.nodes);
 	expect(message).toMatchSnapshot();
-});
-
-describe('find status', () => {
-	const issue = [
-		{
-			dismissedAt: null,
-			securityVulnerability: {
-				severity: 'HIGH',
-				advisory: {
-					severity: 'HIGH',
-				},
-				package: {
-					name: 'lodash',
-				},
-			},
-		},
-	];
-
-	const commit = [{ commit: { messageHeadline: 'fix: something' } }];
-	const commitWithFix = [{ commit: { messageHeadline: 'chore: bump lodash' } }];
-
-	test('oneflow PR', async () => {
-		const message = await findStatus(issue, commit, 'oneflow', 'some title', 'some body');
-		expect(message).toBe('success');
-	});
-
-	test('dependabot[bot] PR', async () => {
-		const message = await findStatus(issue, commit, 'dependabot[bot]', 'some title', 'some body');
-		expect(message).toBe('success');
-	});
-
-	test('empty PR body', async () => {
-		const message = await findStatus(issue, commit, 'user', 'some title', null);
-		expect(message).toBe('failure');
-	});
-
-	test('PR body with fix message', async () => {
-		const message = await findStatus(issue, commit, 'user', 'some title', 'update vulnerable dependencies');
-		expect(message).toBe('success');
-	});
-
-	test('commit with fix message', async () => {
-		const message = await findStatus(issue, commitWithFix, 'user', 'some title', 'some description');
-		expect(message).toBe('success');
-	});
 });
